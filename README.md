17988b600404b5d212acb2058fb226a8c38ebb80
gosha20777 <gosha20777@live.ru>
2023-01-11 00:06:54 +0300

    add frontend layer

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
 # mipt-networks-2022
 Final project for the networks MIPT course
+
+## Face API
+
+API for interaction and control with face recognition engines. This project created with Fast API, Docker, PostgreSQL DB and Krakend API Gateway.
+
+Web UI created using .Net 6 and Web Assebly. 
+
+![example](_docs/img0.png)
+
+## Install
+
+1. Clone this repository
+2. Configure:
+    - `cp .database.example.env .database.env`
+    - `cp app/.log-config.example.ini app/.log-config.ini`
+    - `cp app/.env.example.yaml app/.env.yaml`
+    - `cp jwk/symmetric.example.json jwk/symmetric.json`
+    - `cp krakend/config.example.json krakend/config.json`
+    - for each proider in `app/providers`:
+        - `cp app/providers/$provider/.config.example.yaml app/providers/$provider/.config.yaml`
+3. Build and run:
+    ```bash
+    $ docker compose up -d
+    ```
+
+## Usage
+
+- http://0.0.0.0:5000/docs/v1 - dev endpoint (without jwt)
+- http://0.0.0.0:8080/docs/v1 - prod endpoint
+- http://0.0.0.0:8091/__stats - metrics
+- http://0.0.0.0:80/ - web ui
---
+++ frontend/src/Client/Services/Settings.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using System;
+
+namespace MessengerWeb.Client.Services
+{
+    public class Settings
+    {
+        public List<EngineEntity> Engines { get; set; }
+
+        public Settings()
+        {
+            Engines = new List<EngineEntity> 
+            {
+                new EngineEntity()
+                {
+                    Engine = Engine.Luna,
+                    Name = "Vision Labs Platform",
+                    UUID = "2a2a5e3b-29c6-426f-97cc-d9e1fb701ad3",
+                    IsSelected = true,
+                },
+                new EngineEntity()
+                {
+                    Engine = Engine.Ntech,
+                    Name = "NTech Platform",
+                    UUID = "6cf3b728-4cf9-4262-b4b2-315c019515de",
+                    IsSelected = false,
+                },
+                new EngineEntity()
+                {
+                    Engine = Engine.Tevian,
+                    Name = "Tevian Platform",
+                    UUID = "3aac91d1-319f-46d8-bd52-35693457f498",
+                    IsSelected = false,
+                },
+                new EngineEntity()
+                {
+                    //Facenet and tevian are have the same API
+                    Engine = Engine.Tevian, 
+                    Name = "Facenet (ours)",
+                    UUID = "d733c7a4-7da4-47d4-aa25-289b9a479819",
+                    IsSelected = false,
+                },
+            };
+        }
+    }
+}
